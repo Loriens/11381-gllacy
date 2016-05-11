5be031a03d3ed02828fc16ff1849ef2da39c888c
Loriens <lerd.95@mail.ru>
2016-05-11 12:52:36 +0300

    Fix a few errors

--- js/script.js
+++ js/script.js
@@ -1,25 +1,3 @@
-var feedback = document.querySelector(".feedback");
-var overlay = document.querySelector(".overlay-feedback");
-var feedbackOpen = document.querySelector(".index-map-information a:first-of-type");
-var feedbackClose = feedback.querySelector(".feedback-close");
-
-feedbackOpen.addEventListener("click", function(event) {
-  event.preventDefault();
-  feedback.classList.remove("feedback-hide");
-  overlay.classList.remove("overlay-feedback-hide");
-  feedback.classList.add("feedback-show");
-  overlay.classList.add("overlay-feedback-show");
-});
-
-feedbackClose.addEventListener("click", function(event) {
-  event.preventDefault();
-  feedback.classList.remove("feedback-show");
-  overlay.classList.remove("overlay-feedback-show");
-  feedback.classList.add("feedback-hide");
-  overlay.classList.add("overlay-feedback-hide");
-});
-
-
 var inputs = document.querySelectorAll("input[type='text'], input[type='email'], input[type='password']");
 var textarea = document.querySelector("textarea");
 
@@ -39,4 +17,25 @@ textarea.addEventListener("keyup", function(event) {
   } else {
     event.target.classList.remove("label-hide");
   }
+});
+
+var feedback = document.querySelector(".feedback");
+var overlay = document.querySelector(".overlay-feedback");
+var feedbackOpen = document.querySelector(".index-map-information a:first-of-type");
+var feedbackClose = feedback.querySelector(".feedback-close");
+
+feedbackOpen.addEventListener("click", function(event) {
+  event.preventDefault();
+  feedback.classList.remove("feedback-hide");
+  overlay.classList.remove("overlay-feedback-hide");
+  feedback.classList.add("feedback-show");
+  overlay.classList.add("overlay-feedback-show");
+});
+
+feedbackClose.addEventListener("click", function(event) {
+  event.preventDefault();
+  feedback.classList.remove("feedback-show");
+  overlay.classList.remove("overlay-feedback-show");
+  feedback.classList.add("feedback-hide");
+  overlay.classList.add("overlay-feedback-hide");
 });
