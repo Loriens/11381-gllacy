6d37cb040e9c4673083940e894938a15bcd359b3
Loriens <lerd.95@mail.ru>
2016-05-11 15:19:18 +0300

    Divide scripts.js into two files(scripts.js and index-popup.js)

--- js/script.js
+++ js/script.js
@@ -15,27 +15,8 @@ textarea.addEventListener("keyup", function(event) {
   if(event.target.value) {
     event.target.classList.add("label-hide");
   } else {
-    event.target.classList.remove("label-hide");
+    if(event.target.classList.contain("label-hide")) {
+      event.target.classList.remove("label-hide");
+    }
   }
-});
-
-var feedback = document.querySelector(".feedback");
-var overlay = document.querySelector(".overlay-feedback");
-var feedbackOpen = document.querySelector(".index-map-information a:first-of-type");
-var feedbackClose = feedback.querySelector(".feedback-close");
-
-feedbackOpen.addEventListener("click", function(event) {
-  event.preventDefault();
-  feedback.classList.remove("feedback-hide");
-  overlay.classList.remove("overlay-feedback-hide");
-  feedback.classList.add("feedback-show");
-  overlay.classList.add("overlay-feedback-show");
-});
-
-feedbackClose.addEventListener("click", function(event) {
-  event.preventDefault();
-  feedback.classList.remove("feedback-show");
-  overlay.classList.remove("overlay-feedback-show");
-  feedback.classList.add("feedback-hide");
-  overlay.classList.add("overlay-feedback-hide");
 });
